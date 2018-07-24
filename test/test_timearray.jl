cde7648471f3b312d4670ee92ba47dea99ae2e50
femtotrader <femto.trader@gmail.com>
2018-07-24 08:25:16 +0200

    Update test_timearray.jl

--- test/test_timearray.jl
+++ test/test_timearray.jl
@@ -2,14 +2,16 @@ using TimeSeriesIO
 
 using Base.Test
 
-# Test convert TimeArray to DataFrame
 using TimeSeries
-filename = joinpath(dirname(@__FILE__), "ford_2012.csv")
-ta2 = readtimearray(filename)
-ta2 = ta2["Open", "High", "Low", "Close"]
-df2 = DataFrame(ta2)
-@test names(df2) == [:Date, :Open, :High, :Low, :Close]
 
-## With optional arguments
-df2 = DataFrame(ta2, timestamp=:DateTime, colnames=[:Open, :Close])
-@test names(df2) == [:DateTime, :Open, :Close]
+@testset "TimeArray to DataFrame" begin
+    filename = joinpath(dirname(@__FILE__), "ford_2012.csv")
+    ta2 = readtimearray(filename)
+    ta2 = ta2["Open", "High", "Low", "Close"]
+    df2 = DataFrame(ta2)
+    @test names(df2) == [:Date, :Open, :High, :Low, :Close]
+
+    ## With optional arguments
+    df2 = DataFrame(ta2, timestamp=:DateTime, colnames=[:Open, :Close])
+    @test names(df2) == [:DateTime, :Open, :Close]
+end
